7346d67d894153f61c72a347a1b7978d86613b75
Brot <sepp2017.huber@gmail.com>
2023-01-22 19:38:33 +0100

    Code Update

--- In_Bearbeitung/Buchstaben_zahlen.py
+++ In_Bearbeitung/Buchstaben_zahlen.py
@@ -1,24 +1,44 @@
 '''
-Phillip 17.01.2023
+Phillip 22.01.2023
 '''
 
 
 
 while True:
+    length = 0  # Reset the length variable
+
     choice = input("Do you want to analyse a word, or a file? (w/f/c) ") # Ask the user if they want to analyse a word or a file
 
     if choice == "w": # If they want to analyse a word
         word = input("Enter a word: ") # Ask for the word
-        print(f"Length: {len(word)}") # Print the length of the word
+
+        for i in range(len(word)): # For each letter in the word
+            character = word[i] # Set the variable words to the letter
+
+            if character.isalpha():
+                length += 1 # Add 1 to the length variable
+
+            else:
+                pass
+
+        print(f"Ammount of Letters in the Word: {length}") # Print the amount of letters in the word
 
     elif choice == "f": # If they want to analyse a file
-        length = 0 # Reset the length variable
+
         file_name = input("Enter a file name: ") # Ask for the file name
         file = open(file_name, "r") # Opens the file
         for line in file:
-            length += len(line) # Calculates the length of the file
+            for i in range(len(line)):  # For each letter in the word
+                character = line[i]  # Set the variable words to the letter
+
+                if character.isalpha():
+                    length += 1  # Add 1 to the length variable
+
+                else:
+                    pass
         file.close()
-        print(f"Length: {length}") # Prints the length of the file
+
+        print(f"Ammount of Letters in the Word: {length}") # Prints the amount of letters in the file
 
     elif choice == "c":
         print("Goodbye!")
